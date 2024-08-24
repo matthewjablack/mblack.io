ceda5e0c2cf2e1461b22b6725c8485b92f2f0ed8
Matthew Black <mblackmblackmblack@gmail.com>
2024-08-24 17:38:47 -0400

    fix: alt in ln post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724534546',resources: [
+    cacheName: 'chirpy-1724535517',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
